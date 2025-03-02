92fededa85ee5ee0a66afdc7fd2529035bf07049
george-josephv <georgejosephjv@gmail.com>
2025-03-03 01:00:17 +0530

    profile view and edit

--- homecare/homecare_app/forms.py
+++ homecare/homecare_app/forms.py
@@ -1,6 +1,36 @@
 from django import forms
 from .models import Booking, User
 
+BLOOD_GROUP_CHOICES = [
+    ('A+', 'A+'),
+    ('A-', 'A-'),
+    ('B+', 'B+'),
+    ('B-', 'B-'),
+    ('O+', 'O+'),
+    ('O-', 'O-'),
+    ('AB+', 'AB+'),
+    ('AB-', 'AB-'),
+]
+
+DISTRICT_CHOICES = [
+        ("Thiruvananthapuram", "Thiruvananthapuram"),
+    ("Kollam", "Kollam"),
+    ("Pathanamthitta", "Pathanamthitta"),
+    ("Alappuzha", "Alappuzha"),
+    ("Kottayam", "Kottayam"),
+    ("Idukki", "Idukki"),
+    ("Ernakulam", "Ernakulam"),
+    ("Thrissur", "Thrissur"),
+    ("Palakkad", "Palakkad"),
+    ("Malappuram", "Malappuram"),
+    ("Kozhikode", "Kozhikode"),
+    ("Wayanad", "Wayanad"),
+    ("Kannur", "Kannur"),
+    ("Kasaragod", "Kasaragod"),
+
+]
+
+
 class BookingForm(forms.ModelForm):
     expected_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
 
@@ -8,9 +38,19 @@ class BookingForm(forms.ModelForm):
         model = Booking
         fields = ['expected_date']
 class EditProfileForm(forms.ModelForm):
+    blood_group = forms.ChoiceField(
+        choices=BLOOD_GROUP_CHOICES, 
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    district = forms.ChoiceField(
+        choices=DISTRICT_CHOICES, 
+        widget=forms.Select(attrs={'class': 'form-control'})
+
+    )
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'phone_number', 'dob', 'state', 'district', 'place', 'housename', 'profile_picture']
+        fields = ['username', 'email', 'phone_number', 'dob', 'state', 'district', 'place', 'housename', 'profile_picture','blood_group']
         widgets = {
             'dob': forms.DateInput(attrs={'type': 'date'}),
         }
